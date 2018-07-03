0f93b48cbbe52854a915515258f41768832e21a0
Krait <krait@skbkontur.ru>
2018-07-03 21:51:49 +0500

    Fixed specs.

---
+++ Vostok.Commons.Testing/Testing/AssertionAssertions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using NSubstitute.Exceptions;
+using NUnit.Framework;
+using Vostok.Commons.Conversions;
+using Vostok.Commons.Time;
+
+namespace Vostok.Commons.Testing.Testing
+{
+    public static class AssertionAssertions
+    {
+        public static void ShouldPassIn(this Action assertion, TimeSpan wait)
+        {
+            assertion.ShouldPassIn(wait, 10.Milliseconds());
+        }
+
+        public static void ShouldPassIn(this Action assertion, TimeSpan wait, TimeSpan pause)
+        {
+            var budget = TimeBudget.StartNew(wait);
+
+            while (!budget.HasExpired())
+            {
+                try
+                {
+                    assertion();
+                    return;
+                }
+                catch (AssertionException)
+                {
+                }
+                catch (ReceivedCallsException)
+                {
+                }
+
+                Thread.Sleep(pause);
+            }
+
+            assertion();
+        }
+
+        public static void ShouldNotFailIn(this Action assertion, TimeSpan wait)
+        {
+            ShouldNotFailIn(assertion, wait, 10.Milliseconds());
+        }
+
+        public static void ShouldNotFailIn(this Action assertion, TimeSpan wait, TimeSpan pause)
+        {
+            var budget = TimeBudget.StartNew(wait);
+
+            while (!budget.HasExpired())
+            {
+                assertion();
+
+                Thread.Sleep(pause);
+            }
+        }
+    }
+}
