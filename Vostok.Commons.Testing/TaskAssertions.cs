62516b7da375672184529f62efb3a36691132348
iloktionov <iloktionov@skbkontur.ru>
2018-09-12 16:12:51 +0500

    Added more useful assertions and helpers.

---
+++ Vostok.Commons.Testing/TaskAssertions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Testing
+{
+    [PublicAPI]
+    internal static class TaskAssertions
+    {
+        public static void ShouldCompleteImmediately(this Task task)
+        {
+            task.IsCompleted.Should().BeTrue();
+        }
+
+        public static void ShouldCompleteIn(this Task task, TimeSpan timeout)
+        {
+            task.Wait(timeout).Should().BeTrue();
+        }
+
+        public static void ShouldCompleteWithErrorIn<TException>(this Task task, TimeSpan timeout)
+            where TException : Exception
+        {
+            Action action = () => task.Wait(timeout);
+
+            action.Should().Throw<AggregateException>().WithInnerException<TException>().Which.ShouldBePrinted();
+        }
+
+        public static void ShouldNotCompleteIn(this Task task, TimeSpan timeout)
+        {
+            task.Wait(timeout).Should().BeFalse();
+        }
+    }
+}
