02a25751601d347da45cf491b930f14fa5c0b934
iloktionov <iloktionov@skbkontur.ru>
2018-09-12 16:12:51 +0500

    Added more useful assertions and helpers.

---
+++ Vostok.Commons.Testing/ConditionAssertions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Testing
+{
+    [PublicAPI]
+    internal static class ConditionAssertions
+    {
+        public static void ShouldBecomeTrueIn(this Func<bool> condition, TimeSpan wait)
+        {
+            condition.ShouldBecomeTrueIn(wait, 10.Milliseconds());
+        }
+
+        public static void ShouldBecomeTrueIn(this Func<bool> condition, TimeSpan wait, TimeSpan pause)
+        {
+            var watch = Stopwatch.StartNew();
+
+            while (watch.Elapsed < wait)
+            {
+                if (condition())
+                    return;
+
+                Thread.Sleep(pause);
+            }
+
+            condition().Should().BeTrue("because given condition should have become true in {0}", wait);
+        }
+
+        public static void ShouldNotBecomeTrueIn(this Func<bool> condition, TimeSpan wait)
+        {
+            ShouldNotBecomeTrueIn(condition, wait, 10.Milliseconds());
+        }
+
+        public static void ShouldNotBecomeTrueIn(this Func<bool> condition, TimeSpan wait, TimeSpan pause)
+        {
+            var watch = Stopwatch.StartNew();
+
+            while (watch.Elapsed < wait)
+            {
+                condition().Should().BeFalse("because given condition shouldn't have become true in {0}", wait);
+
+                Thread.Sleep(pause);
+            }
+        }
+    }
+}
---
+++ Vostok.Commons.Testing/ExceptionAssertions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Testing
+{
+    [PublicAPI]
+    internal static class ExceptionAssertions
+    {
+        public static void ShouldBePrinted<T>(this T exception)
+            where T : Exception
+        {
+            Console.Out.WriteLine(exception);
+        }
+    }
+}
