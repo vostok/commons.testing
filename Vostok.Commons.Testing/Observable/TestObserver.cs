aebb1d4d043f238062dec342b9dcdfd45216ba19
alexz <alexz@skbkontur.ru>
2018-12-24 15:14:48 +0500

    Added assertions and helpers for observables.

---
+++ Vostok.Commons.Testing/Observable/TestObserver.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using JetBrains.Annotations;
+
+namespace Vostok.Commons.Testing.Observable
+{
+    [PublicAPI]
+    internal class TestObserver<T> : IObserver<T>
+    {
+        public IList<Notification<T>> Messages
+        {
+            get
+            {
+                lock (lockObject)
+                    return messages.ToList();
+            }
+        }
+
+        public IList<T> Values => Messages.Select(message => message.Value).ToList();
+        
+        private readonly object lockObject = new object();
+        private readonly List<Notification<T>> messages = new List<Notification<T>>();
+
+        public void OnNext(T value)
+        {
+            Add(Notification.CreateOnNext(value));
+        }
+
+        public void OnError(Exception exception)
+        {
+            Add(Notification.CreateOnError<T>(exception));
+        }
+
+        public void OnCompleted()
+        {
+            Add(Notification.CreateOnCompleted<T>());
+        }
+
+        private void Add(Notification<T> message)
+        {
+            lock (lockObject)
+                messages.Add(message);
+        }
+    }
+}
