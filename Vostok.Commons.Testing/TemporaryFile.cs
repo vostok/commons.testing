ab410cb5a098f67198131dd37d186893e9bd0d0b
kungurtsev <kungurtsev@skbkontur.ru>
2019-03-13 14:31:55 +0500

    code style

--- Vostok.Commons.Testing/TemporaryFile.cs
+++ Vostok.Commons.Testing/TemporaryFile.cs
@@ -7,15 +7,15 @@ namespace Vostok.Commons.Testing
     [PublicAPI]
     internal class TemporaryFile : IDisposable
     {
-        public string FileName { get; }
-        
         public TemporaryFile(string content = null)
         {
             FileName = Path.GetTempFileName();
             if (content != null)
                 File.WriteAllText(FileName, content);
         }
-        
+
+        public string FileName { get; }
+
         public void Dispose()
         {
             if (File.Exists(FileName))
